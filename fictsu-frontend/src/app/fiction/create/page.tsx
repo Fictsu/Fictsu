a7e0fc906a281b47411233b43951ea602ef44fbb
kwangsensei <kwangsensei@outlook.com>
2025-03-30 02:07:27 +0700

    Fix typo

--- fictsu-frontend/src/app/fiction/create/page.tsx
+++ fictsu-frontend/src/app/fiction/create/page.tsx
@@ -18,7 +18,7 @@ export default function FictionCreatePage() {
     const [cover, setCover] = useState("/default-cover.png")
     const [fiction, setFiction] = useState<Fiction | null>(null)
 
-    const { register: registerFiction, handleSubmit: handleFictionSubmit, control: controlFiction, formState: { errors } } = useForm<FictionForm>()
+    const { register, handleSubmit, control, formState: { errors } } = useForm<FictionForm>()
 
     const onSubmit = async (data: FictionForm) => {
         setLoading(true)
@@ -58,7 +58,7 @@ export default function FictionCreatePage() {
         <div className="max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-lg mt-10 mb-10 border border-gray-200">
             <h1 className="text-2xl font-semibold text-gray-900 mb-6 text-center">Create Fiction</h1>
 
-            <form onSubmit={handleFictionSubmit(onSubmit)} className="space-y-6">
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
                 <div className="flex gap-7">
                     <div className="relative rounded-lg shadow-md cursor-pointer group">
                         <Image
@@ -83,21 +83,21 @@ export default function FictionCreatePage() {
                     <div className="flex-1 space-y-4">
                         <div className="space-y-1">
                             <input
-                                {...registerFiction("title", { required: "Title is required" })}
+                                {...register("title", { required: "Title is required" })}
                                 placeholder={errors.title ? errors.title.message : "Title"}
                                 className={`w-full p-4 border rounded-lg focus:outline-none focus:ring-2 ${errors.title ? "text-red-500 border-red-500 focus:ring-red-500" : "border-gray-300 focus:ring-blue-500"} text-gray-900`}
                             />
                         </div>
 
                         <input
-                            {...registerFiction("subtitle")}
+                            {...register("subtitle")}
                             placeholder="Subtitle"
                             className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-900"
                         />
 
                         <div className="space-y-1">
                             <input
-                                {...registerFiction("author", { required: "Author is required" })}
+                                {...register("author", { required: "Author is required" })}
                                 placeholder={errors.author ? errors.author.message : "Author"}
                                 className={`w-full p-4 border rounded-lg focus:outline-none focus:ring-2 ${errors.author ? "text-red-500 border-red-500 focus:ring-red-500" : "border-gray-300 focus:ring-blue-500"} text-gray-900`}
                             />
@@ -105,14 +105,14 @@ export default function FictionCreatePage() {
 
                         <div className="space-y-1">
                             <input
-                                {...registerFiction("artist", { required: "Artist is required" })}
+                                {...register("artist", { required: "Artist is required" })}
                                 placeholder={errors.artist ? errors.artist.message : "Artist *If no artsis, type N/A"}
                                 className={`w-full p-4 border rounded-lg focus:outline-none focus:ring-2 ${errors.artist ? "text-red-500 border-red-500 focus:ring-red-500" : "border-gray-300 focus:ring-blue-500"} text-gray-900`}
                             />
                         </div>
 
                         <select
-                            {...registerFiction("status")}
+                            {...register("status")}
                             className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-900 pl-3"
                         >
                             <option value="Ongoing">Ongoing</option>
@@ -130,7 +130,7 @@ export default function FictionCreatePage() {
                     <div className={`border rounded-lg p-2 ${errors.synopsis ? "border-red-500" : "border-gray-300"} text-gray-900`}>
                         <Controller
                             name="synopsis"
-                            control={controlFiction}
+                            control={control}
                             rules={{
                                 required: "Synopsis is required",
                                 validate: value => value.replace(/<(.|\n)*?>/g, '').trim() !== "" || "Synopsis is required"
