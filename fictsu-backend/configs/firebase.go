39255c8337aa491232ecfbf3e3b93ce36f3535c7
kwangsensei <kwangsensei@outlook.com>
2025-04-01 20:01:57 +0700

    Refactored

--- fictsu-backend/configs/firebase.go
+++ fictsu-backend/configs/firebase.go
@@ -1,20 +1,21 @@
 package config
 
 import (
-	"context"
 	"log"
+	"context"
 
-	firebase "firebase.google.com/go/v4"
 	"google.golang.org/api/option"
+	firebase "firebase.google.com/go/v4"
 )
 
 var FirebaseApp *firebase.App
 
 func InitFirebaseApp() {
-	opt := option.WithCredentialsFile("configs/firebaseCre.json")
+	opt := option.WithCredentialsFile("configs/firebase-create.json")
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
-		log.Fatalf("error initializing Firebase app: %v", err)
+		log.Fatalf("Error initializing Firebase app: %v", err)
 	}
+
 	FirebaseApp = app
 }
