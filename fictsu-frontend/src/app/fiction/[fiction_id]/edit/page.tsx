c79e2a579fb264c98ee1b56d8f3687a499c2e22f
kwangsensei <kwangsensei@outlook.com>
2025-04-23 15:11:10 +0700

    Refactored

--- fictsu-frontend/src/app/fiction/[fiction_id]/edit/page.tsx
+++ fictsu-frontend/src/app/fiction/[fiction_id]/edit/page.tsx
@@ -12,32 +12,55 @@ import ChapterActions from "@/components/ChapterActions"
 import { Fiction, FictionForm, Chapter } from "@/types/types"
 import FloatingToolsMenu from "@/components/FloatingToolsMenu"
 
-// Dynamically import ReactQuill to avoid SSR issues
 const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false })
 const fetcher = (URL: string) => fetch(URL, { credentials: "include" }).then((res) => res.json())
 
-function DataHandler(dataStr: any, DATA_TAG: any){
-    const formData = new FormData()
-    console.log(DATA_TAG.length)
-    for(let i=0; i< DATA_TAG.length; i++){
-        formData.append(DATA_TAG[i], dataStr[i])
-    }
-    return formData
-}
-
 export default function FictionEditPage({ params }: { params: Promise<{ fiction_id: string }> }) {
     const router = useRouter()
-    const { fiction_id } = use(params)
-    const fictionIDNumber = Number(fiction_id)
 
     const [loading, setLoading] = useState(false)
     const [cover, setCover] = useState("/default-cover.png")
     const [coverFile, setCoverFile] = useState<File | null>(null)
 
+    const { fiction_id } = use(params)
+    const fictionIDNumber = Number(fiction_id)
     const { register, handleSubmit, reset, control, formState: { errors } } = useForm<FictionForm>()
+    const { data: userData, error: userError } = useSWR(
+        `${process.env.NEXT_PUBLIC_BACKEND_API}/user`, fetcher
+    )
+    const { data: fictionData, error: fictionError, mutate } = useSWR<{ Fiction: Fiction }>(
+        `${process.env.NEXT_PUBLIC_BACKEND_API}/f/${fiction_id}`, fetcher
+    )
 
-    const { data: userData, error: userError } = useSWR(`${process.env.NEXT_PUBLIC_BACKEND_API}/user`, fetcher)
-    const { data: fictionData, error: fictionError, mutate } = useSWR<{ Fiction: Fiction }>(`${process.env.NEXT_PUBLIC_BACKEND_API}/f/${fiction_id}`, fetcher)
+    const handleCoverChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0] || null
+        setCoverFile(file)
+        if (file) {
+            const reader = new FileReader()
+            reader.onloadend = () => setCover(reader.result as string)
+            reader.readAsDataURL(file)
+        }
+    }
+
+    const onSubmit = async (formData: FictionForm) => {
+        setLoading(true)
+        const formDataObj = new FormData()
+        formDataObj.append("cover", coverFile || "")
+        Object.entries(formData).forEach(([key, value]) => formDataObj.append(key, value))
+        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/f/${fiction_id}/u`, {
+            method: "PUT",
+            credentials: "include",
+            body: formDataObj,
+        })
+
+        setLoading(false)
+        if (!response.ok) {
+            return alert("Failed to update fiction.")
+        }
+
+        alert("Fiction updated successfully!")
+        mutate()
+    }
 
     useEffect(() => {
         if (userError || fictionError) {
@@ -60,37 +83,6 @@ export default function FictionEditPage({ params }: { params: Promise<{ fiction_
         setCover(fictionData.Fiction.cover || "/default-cover.png")
     }, [userData, fictionData, fictionError, userError, reset, router])
 
-    const onSubmit = async (formData: FictionForm) => {
-        setLoading(true)
-        const formDataObj = new FormData()
-        formDataObj.append("cover", coverFile || "")
-        Object.entries(formData).forEach(([key, value]) => formDataObj.append(key, value))
-
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API}/f/${fiction_id}/u`, {
-            method: "PUT",
-            credentials: "include",
-            body: formDataObj,
-        })
-
-        setLoading(false)
-        if (!response.ok) {
-            return alert("Failed to update fiction.")
-        }
-
-        alert("Fiction updated successfully!")
-        mutate()
-    }
-
-    const handleCoverChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0] || null
-        setCoverFile(file)
-        if (file) {
-            const reader = new FileReader()
-            reader.onloadend = () => setCover(reader.result as string)
-            reader.readAsDataURL(file)
-        }
-    }  
-
     if (!fictionData || !userData) {
         return <p className="text-center mt-10">Loading...</p>
     }
@@ -98,20 +90,23 @@ export default function FictionEditPage({ params }: { params: Promise<{ fiction_
     return (
         <div className="max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-lg mt-10 mb-10 border border-gray-200">
             <h1 className="text-2xl font-semibold text-gray-900 mb-6 text-center">Edit Fiction</h1>
+
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
                 <div className="flex gap-7">
                     <div className="relative rounded-lg shadow-md cursor-pointer group">
                         <Image
                             src={cover}
-                            alt="Fiction Cover"
                             width={230}
                             height={300}
+                            alt="Fiction Cover"
                             className="rounded-lg object-cover"
                         />
+
                         {/* Hover overlay */}
                         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-end justify-center opacity-0 group-hover:opacity-35 transition-opacity pb-34 rounded-lg">
                             <span className="text-white text-sm font-semibold">Upload Cover Image</span>
                         </div>
+
                         {/* Hidden File Input */}
                         <input
                             type="file"
@@ -120,6 +115,7 @@ export default function FictionEditPage({ params }: { params: Promise<{ fiction_
                             className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
                         />
                     </div>
+
                     <div className="flex-1 space-y-4">
                         <div className="space-y-1">
                             <input
@@ -167,6 +163,7 @@ export default function FictionEditPage({ params }: { params: Promise<{ fiction_
                     <label className={`block font-medium ${errors.synopsis ? "text-red-500" : "text-gray-700"}`}>
                         {errors.synopsis ? errors.synopsis.message : "Synopsis"}
                     </label>
+
                     <div className={`border rounded-lg p-2 ${errors.synopsis ? "border-red-500" : "border-gray-300"} text-gray-900`}>
                         <Controller
                             name="synopsis"
@@ -202,17 +199,25 @@ export default function FictionEditPage({ params }: { params: Promise<{ fiction_
 
             <div className="mt-6">
                 <h2 className="text-xl font-semibold text-gray-800">Chapters</h2>
+
                 {fictionData.Fiction.chapters && fictionData.Fiction.chapters.length > 0 ? (
                     <ul className="mt-4 space-y-3">
                         {fictionData.Fiction.chapters.map((chapter: Chapter) => (
                             <li key={chapter.id} className="flex items-center justify-between bg-gray-50 p-4 rounded-lg shadow-md">
+
                                 <div className="flex-1">
                                     <Link href={`/f/${fiction_id}/${chapter.id}`} className="text-blue-600 hover:underline">
                                         {chapter.title}
                                     </Link>
                                     <p className="text-sm text-gray-600">Created on {new Date(chapter.created).toLocaleDateString()}</p>
                                 </div>
-                                <ChapterActions fictionID={fictionIDNumber} chapterID={chapter.id} contributorID={fictionData.Fiction.contributor_id} />
+
+                                <ChapterActions
+                                    fictionID={fictionIDNumber}
+                                    chapterID={chapter.id}
+                                    contributorID={fictionData.Fiction.contributor_id}
+                                    mutate={mutate}
+                                />
                             </li>
                         ))}
                     </ul>
